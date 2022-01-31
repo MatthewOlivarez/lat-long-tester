08ce9efdbc73bb91896aca453881440f02d467ae
Matthew Olivarez <29942482+MatthewOlivarez@users.noreply.github.com>
2022-01-30 19:40:07 -0600

    Add files via upload

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,6 +1,7 @@
-let requestOptions = {
-    method: 'GET',
-  }
+// let requestOptions = 
+// {
+//     method: 'POST',
+// }
 let lat = document.getElementById("latitude")
 let long = document.getElementById("longitude")
 let formSubmit = document.getElementById("formSubmit")
@@ -9,18 +10,18 @@ let obj;
 
   
   
-formSubmit.addEventListener("submit", () => {
-    fetch(`https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${long}&apiKey=6650f1fc646f45b9ac9f90d91eec2078`, requestOptions)
+formSubmit.addEventListener('submit', () => {
+    fetch("/search")
         .then(response => response.json())
-        .then(result => console.log(result))
+        .then(result => obj = result)
         .catch(error => console.log('error', error));
-
-        window.alert()
 })
+
+
 
 // fetch(`https://api.geoapify.com/v1/geocode/reverse?lat=63.668281&lon=-43.871982&apiKey=6650f1fc646f45b9ac9f90d91eec2078`, requestOptions)
 //     .then(response => response.json())
-//     .then(result => console.log(result))
+//     .then(result => obj = result)
 //     .catch(error => console.log('error', error));
 
 // var axios = require('axios');
