a069333a59eef1d6bec7a00f464ad56bd1867e1e
Matthew Olivarez <29942482+MatthewOlivarez@users.noreply.github.com>
2022-02-15 20:58:03 -0600

    Add files via upload
    added comments

--- app.js
+++ app.js
@@ -1,28 +1,44 @@
-const express = require('express')
+/*
+Server file for informational page representation
+Using Node.js and Express for server
+Software Engineering CS3340
+Spring 2022
+*/
+const express = require('express') // imports express into express variable
 const axios = require('axios')
 const app = express()
 const path = require('path')
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000 // saving port to be used in server into variable
 
 
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(express.static(path.join(__dirname + '/views')))
+app.set('view engine', 'ejs') // sets engine for ejs formatting 
+app.use(express.urlencoded({ extended: true })) // parses requests in JSON format
+app.use(express.json()) // similar to above, ensures we use JSON format for data from api's
+
+ // tells express to search for static files in views directory, static files are html files
+app.use(express.static(path.join(__dirname + '/views'))) 
 //app.set('views', path.join(__dirname, '/views'))
-app.use(express.static(path.join(__dirname, 'public')))
+
+// similar to above, tells express to search for static files in public directory
+// in this case, static files are css and javascript
+app.use(express.static(path.join(__dirname + '/public')))
 
 
 
-let newsStories = []
-let newsCollected = true
-let countryCode;
+let newsStories = [] // this will be array of objects to store news stories
+let newsCollected = true 
+let countryCode // will store country code returned from geoapify api
+let countryName // will store country name returned from geoapify api
 
+
+// "get" route that targets our home/landing page, no matter the result fo the if statement, we render the index file in views directory
 app.get('/', (req, res) => {
     if (newsStories.length > 0)
     {
         newsCollected = true
         res.render('index', { newsCollected })
+        //res.send
+        //res.redirect
     }
     else
     {
@@ -31,6 +47,12 @@ app.get('/', (req, res) => {
     }
 })
 
+
+// "post" route that handles most of our logic
+// pulls latitude/longitude and serves it into axios "get" request to geoapify
+// that returns country code that is served into subsequent axios "get" request to newsapi
+// that returns our news stories that will be "pushed" or appended into array
+// finally we redirect into results route
 app.post('/search', async (req, res) => {
     const { latitude, longitude } = req.body
     //console.log(latitude + " " + longitude)
@@ -39,6 +61,7 @@ app.post('/search', async (req, res) => {
         .then( (response) => {
             //console.log(response.data.features[0].properties.country_code)
             countryCode = response.data.features[0].properties.country_code;
+            countryName = response.data.features[0].properties.country;
         })
         .catch( (error) => {
             // handle error
@@ -59,22 +82,17 @@ app.post('/search', async (req, res) => {
             console.log(error);
         })
     //res.send('worked')
-    res.redirect('/results')
+    res.redirect('/results') // redirects to results route, which is below
 })
 
+
+// "get" route that targets our results page, and will render results file from views directory
 app.get('/results', (req, res) => {
     //res.render('results')
-    res.render('results', { newsStories })
+    res.render('results', { newsStories, countryName }) // renders results, { newsStories, countryName } this will pass the newsStories array and country name variable into the file using the ejs package, which is for templating and sending data into files
 })
 
-
-app.listen(3000, () =>
+// starts server, console.log is to ensure things worked properly
+app.listen(port, () =>
     console.log(`App listening at http://localhost:${port}`)
 );
-
-// formSubmit.addEventListener("submit", () => {
-//     fetch(`https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${long}&apiKey=6650f1fc646f45b9ac9f90d91eec2078`, requestOptions)
-//         .then(response => response.json())
-//         .then(result => obj = result)
-//         .catch(error => console.log('error', error))
-// })
