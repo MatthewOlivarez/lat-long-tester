7a179f8b7434fbfb52e64224bfc895ee803a7e6b
Matthew Olivarez <29942482+MatthewOlivarez@users.noreply.github.com>
2022-02-15 20:58:52 -0600

    Add files via upload
    added dark mode functionality
    added comments

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,15 +1,14 @@
-// let requestOptions = 
-// {
-//     method: 'POST',
-// }
-let lat = document.getElementById("latitude")
-let long = document.getElementById("longitude")
-let formSubmit = document.getElementById("formSubmit")
+let lat = document.getElementById("latitude") // grabs html tag with id of latitude
+let long = document.getElementById("longitude") // grabs html tag with id of longitude
+let formSubmit = document.getElementById("formSubmit") // grabs html tag with id of formSubmit
+let darkModeButton = document.getElementById("darkMode") // grabs html tag with id of darkMode
+let body = document.body // grabs entire body of html page, not necessary but makes it easier to just type body
 let obj;
 
 
   
-  
+// connects our form so that when submit button is clicked 
+// it tells express to send the post route, which begins all of the backend data retrieval
 formSubmit.addEventListener('submit', () => {
     fetch("/search")
         .then(response => response.json())
@@ -18,24 +17,25 @@ formSubmit.addEventListener('submit', () => {
 })
 
 
+// similar event listener to above
+// only for activating dark mode
+// unnecessary, but could transfer over to final product
+darkModeButton.addEventListener('click', () => {
+    if (body.style.backgroundColor == "white")
+    {
+        console.log("Clicked to Dark Mode")
+        body.style.backgroundColor = "#121212" // as an example, body is the variable described above, style is property, in this case that property is basically the css functionality, and background color is a further property within style
+        body.style.color = "white"
+        darkModeButton.innerText = "Light Mode"
+    } 
+    else
+    {
+        console.log("Clicked to Light Mode")
+        body.style.backgroundColor = "white"
+        body.style.color = "black"
+        darkModeButton.innerText = "Dark Mode"
+    }
+    
+})
 
-// fetch(`https://api.geoapify.com/v1/geocode/reverse?lat=63.668281&lon=-43.871982&apiKey=6650f1fc646f45b9ac9f90d91eec2078`, requestOptions)
-//     .then(response => response.json())
-//     .then(result => obj = result)
-//     .catch(error => console.log('error', error));
 
-// var axios = require('axios');
-
-// var config = {
-//   method: 'get',
-//   url: 'https://api.geoapify.com/v1/geocode/reverse?lat=51.21709661403662&lon=6.7782883744862374&apiKey=6650f1fc646f45b9ac9f90d91eec2078',
-//   headers: { }
-// };
-
-// axios(config)
-// .then(function (response) {
-//   console.log(response.data);
-// })
-// .catch(function (error) {
-//   console.log(error);
-// });
